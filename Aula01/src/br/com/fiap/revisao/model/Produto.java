5659656274eed1cda74a2ae5e6f220a971bc0093
Thiago Yamamoto <thiagoyama@gmail.com>
2024-02-09 22:10:16 -0300

    Revisao Java

--- Aula01/src/br/com/fiap/revisao/model/Produto.java
+++ Aula01/src/br/com/fiap/revisao/model/Produto.java
@@ -2,20 +2,33 @@ package br.com.fiap.revisao.model;
 
 public class Produto {
 
-    private  Long id;
+    private Long id;
     private String nome;
     private Double preco;
 
     //new Produto();
-    public Produto(){
-        super();
+    public Produto() {
     }
 
     //new Produto(10, 100.0, "Churros");
-    public Produto(Long id, Double preco, String nome){
+    public Produto(Long id, Double preco, String nome) {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
+    }
+
+    //Calcula o preço final com desconto em %, 10% 20%...
+    public double calcularDesconto(double porcentagem) {
+        return preco - preco * porcentagem / 100;
+    }
+
+    //Sobrecarga de método: método com o mesmo nome mas parâmetros diferentes.
+    //Calcular o preço final com o desconto com cupom "FIAP20" fornece 20% de desconto
+    public double calcularDesconto(String cupom) {
+        if (cupom.equals("FIAP20")) {
+            return preco * 0.8; //calcularDesconto(20);
+        }
+        return preco;
     }
 
     public String getNome() {
@@ -34,11 +47,11 @@ public class Produto {
         this.preco = preco;
     }
 
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getId(){
+    public Long getId() {
         return id;
     }
 
