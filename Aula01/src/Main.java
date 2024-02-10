8c066b83fba7654b194656d86ee616b51d776724
Thiago Yamamoto <thiagoyama@gmail.com>
2024-02-09 22:54:17 -0300

    Revisao Java

--- Aula01/src/Main.java
+++ Aula01/src/Main.java
@@ -1,7 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import br.com.fiap.revisao.model.Produto;
+
 public class Main {
     public static void main(String[] args) {
+        //Instanciar um Produto com dados
+        Produto produto = new Produto(1l, 100.0, "Garrafa");
+        //Chamar o método de calcular desconto
+        double novoValor = produto.calcularDesconto(50);
+        //Exibir o valor com desconto
+        System.out.println("Preço com desconto " + novoValor);
+
+        //Instanciar um Calcado
+        //Chamar o método de calcular desconto
+        //Exibir o valor com desconto
 
     }
 }
--- Aula01/src/br/com/fiap/revisao/model/Calcado.java
+++ Aula01/src/br/com/fiap/revisao/model/Calcado.java
@@ -12,6 +12,17 @@ public class Calcado extends Produto {
         this.tamanho = tamanho;
     }
 
+    //Sobrescrita de método (Override) com o cupom "CALCADO40" que dá 40% de desconto
+    //Além do outro cupom "FIAP20"
+    @Override
+    public double calcularDesconto(String cupom){
+        if (cupom.equals("CALCADO40")){
+            return getPreco() * 0.6;
+        }
+        //Chama o método de calcular desconto do pai
+        return super.calcularDesconto(cupom);
+    }
+
     public Float getTamanho() {
         return tamanho;
     }
