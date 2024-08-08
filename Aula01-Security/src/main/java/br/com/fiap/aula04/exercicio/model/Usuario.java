57333a4a5a9e853c856bf96b9cae6313c162b7a1
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-07 22:47:59 -0300

    Spring Security

---
+++ Aula01-Security/src/main/java/br/com/fiap/aula04/exercicio/model/Usuario.java
@@ -0,0 +1,62 @@
+package br.com.fiap.aula04.exercicio.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="TB_API_USUARIO")
+@Getter @Setter @NoArgsConstructor
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="ds_login", nullable = false, unique = true, length = 50)
+    private String login;
+
+    @Column(name="ds_senha", nullable = false)
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ Aula01-Security/src/main/java/br/com/fiap/aula04/exercicio/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.com.fiap.aula04.exercicio.repository;
+
+import br.com.fiap.aula04.exercicio.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+
+}
